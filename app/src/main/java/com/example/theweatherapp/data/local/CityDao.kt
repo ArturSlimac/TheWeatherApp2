6165a601e74f67c52b38328249c792a87bcdf279
ArturSlimac <artur.nenakhov@student.hogent.be>
2024-08-13 14:19:51 +0200

    Dao's kDoc

--- app/src/main/java/com/example/theweatherapp/data/local/CityDao.kt
+++ app/src/main/java/com/example/theweatherapp/data/local/CityDao.kt
@@ -7,20 +7,51 @@ import androidx.room.Query
 import com.example.theweatherapp.domain.model.city.CityItemEntity
 import kotlinx.coroutines.flow.Flow
 
+/**
+ * Data Access Object (DAO) for managing city-related operations in the local database.
+ *
+ * This interface provides methods for inserting, querying, and deleting city items.
+ */
 @Dao
 interface CityDao {
+    /**
+     * Inserts a [CityItemEntity] into the database.
+     *
+     * If the city already exists in the database (determined by the primary key), it will be replaced.
+     *
+     * @param cityItem The city item to be inserted.
+     * @return The row ID of the inserted item.
+     */
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun insertCityItem(cityItem: CityItemEntity): Long
 
+    /**
+     * Retrieves all cities from the database.
+     *
+     * @return A [Flow] that emits a list of [CityItemEntity] objects representing all cities in the database.
+     */
     @Query("SELECT * FROM city")
     fun getAllCities(): Flow<List<CityItemEntity>>
 
+    /**
+     * Deletes a city from the database based on the provided name and country.
+     *
+     * @param name The name of the city to delete.
+     * @param country The country of the city to delete.
+     */
     @Query("DELETE FROM city WHERE name = :name AND country = :country")
     suspend fun deleteCity(
         name: String,
         country: String,
     )
 
+    /**
+     * Checks if a city exists in the database based on the provided name and country.
+     *
+     * @param name The name of the city to check.
+     * @param country The country of the city to check.
+     * @return `true` if the city exists in the database, `false` otherwise.
+     */
     @Query("SELECT EXISTS(SELECT 1 FROM city WHERE name = :name AND country = :country LIMIT 1)")
     fun isCitySaved(
         name: String,
--- app/src/main/java/com/example/theweatherapp/data/local/WeatherDao.kt
+++ app/src/main/java/com/example/theweatherapp/data/local/WeatherDao.kt
@@ -13,8 +13,24 @@ import com.example.theweatherapp.domain.model.weather.HourlyUnitsEntity
 import com.example.theweatherapp.domain.model.weather.WeatherEntity
 import kotlinx.coroutines.flow.Flow
 
+/**
+ * Data Access Object (DAO) for managing weather-related operations in the local database.
+ *
+ * This interface provides methods for inserting and querying weather data,
+ * including current weather, hourly weather, and their associated units.
+ */
 @Dao
 interface WeatherDao {
+    /**
+     * Retrieves the cached weather data, including associated details, for a specific city.
+     *
+     * This method performs a database transaction to ensure the consistency of the data retrieved.
+     *
+     * @param name The name of the city for which to retrieve the weather data.
+     * @param country The country of the city for which to retrieve the weather data.
+     * @return A [Flow] emitting a [WeatherWithDetails] object containing the weather data and its details,
+     * or `null` if no data is found for the specified city.
+     */
     @Transaction
     @Query("SELECT * FROM weather WHERE cityId IN (SELECT id FROM city WHERE name = :name AND country = :country)")
     fun getCachedWeather(
@@ -22,18 +38,54 @@ interface WeatherDao {
         country: String,
     ): Flow<WeatherWithDetails?>
 
+    /**
+     * Inserts the main weather data into the database.
+     *
+     * If the weather data already exists (based on the primary key), it will be replaced.
+     *
+     * @param weather The [WeatherEntity] object representing the weather data to be inserted.
+     * @return The row ID of the inserted weather data.
+     */
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun insertWeather(weather: WeatherEntity): Long
 
+    /**
+     * Inserts the current weather data into the database.
+     *
+     * If the current weather data already exists (based on the primary key), it will be replaced.
+     *
+     * @param currentWeather The [CurrentEntity] object representing the current weather data to be inserted.
+     */
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun insertCurrentWeather(currentWeather: CurrentEntity)
 
+    /**
+     * Inserts the units associated with the current weather data into the database.
+     *
+     * If the units data already exists (based on the primary key), it will be replaced.
+     *
+     * @param currentUnits The [CurrentUnitsEntity] object representing the units for the current weather to be inserted.
+     */
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun insertCurrentUnits(currentUnits: CurrentUnitsEntity)
 
+    /**
+     * Inserts the hourly weather data into the database.
+     *
+     * If the hourly weather data already exists (based on the primary key), it will be replaced.
+     *
+     * @param hourlyWeather The [HourlyEntity] object representing the hourly weather data to be inserted.
+     */
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun insertHourlyWeather(hourlyWeather: HourlyEntity)
 
+    /**
+     * Inserts the units associated with the hourly weather data into the database.
+     *
+     * If the units data already exists (based on the primary key), it will be replaced.
+     *
+     * @param hourlyUnits The [HourlyUnitsEntity] object representing the units for the hourly weather to be inserted.
+     */
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun insertHourlyUnits(hourlyUnits: HourlyUnitsEntity)
 }

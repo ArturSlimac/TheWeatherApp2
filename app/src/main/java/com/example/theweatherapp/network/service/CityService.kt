043954a0d152fee44db34ff7f66c4a96077c2034
ArturSlimac <artur.nenakhov@student.hogent.be>
2024-08-13 17:06:59 +0200

    Services kDoc

--- app/src/main/java/com/example/theweatherapp/network/service/CityService.kt
+++ app/src/main/java/com/example/theweatherapp/network/service/CityService.kt
@@ -5,7 +5,20 @@ import retrofit2.http.GET
 import retrofit2.http.Header
 import retrofit2.http.Query
 
+/**
+ * Service interface for interacting with the city-related API endpoints.
+ *
+ * This interface provides methods to retrieve city information based on coordinates or name.
+ */
 interface CityService {
+    /**
+     * Retrieves city information based on the provided latitude and longitude coordinates.
+     *
+     * @param latitude The latitude of the location.
+     * @param longitude The longitude of the location.
+     * @param apiKey The API key for authentication.
+     * @return A [CityModel] containing city information corresponding to the provided coordinates.
+     */
     @GET("v1/reversegeocoding")
     suspend fun getCityByCoordinates(
         @Query("lat") latitude: Double,
@@ -13,6 +26,13 @@ interface CityService {
         @Header("X-Api-Key") apiKey: String,
     ): CityModel
 
+    /**
+     * Retrieves a list of cities matching the provided name.
+     *
+     * @param name The name of the city to search for.
+     * @param apiKey The API key for authentication.
+     * @return A [CityModel] containing a list of cities that match the provided name.
+     */
     @GET("v1/geocoding")
     suspend fun getCitiesByName(
         @Query("city") name: String,
--- app/src/main/java/com/example/theweatherapp/network/service/WeatherService.kt
+++ app/src/main/java/com/example/theweatherapp/network/service/WeatherService.kt
@@ -4,7 +4,23 @@ import com.example.theweatherapp.domain.model.weather.WeatherModel
 import retrofit2.http.GET
 import retrofit2.http.Query
 
+/**
+ * Service interface for retrieving weather data from the weather API.
+ *
+ * This interface provides methods to fetch current and hourly weather information
+ * based on geographical coordinates, preferred units, and timezone.
+ */
 interface WeatherService {
+    /**
+     * Fetches the weather forecast for the specified location and units.
+     *
+     * @param latitude The latitude of the location for which to fetch the weather data.
+     * @param longitude The longitude of the location for which to fetch the weather data.
+     * @param temperatureUnit The unit for temperature (e.g., "Celsius", "Fahrenheit").
+     * @param windSpeedUnit The unit for wind speed (e.g., "m/s", "km/h").
+     * @param timezone The timezone in which the forecast times should be expressed.
+     * @return A [WeatherModel] containing the weather forecast data for the specified location.
+     */
     @GET(
         "v1/forecast?current=temperature_2m,relative_humidity_2m,apparent_temperature,weather_code,pressure_msl,wind_speed_10m&hourly=temperature_2m,weather_code&forecast_days=1",
     )

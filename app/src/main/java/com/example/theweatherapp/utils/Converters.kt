63919ebe567cc9ce6d0d424b761a753e103c0ae7
ArturSlimac <artur.nenakhov@student.hogent.be>
2024-08-14 12:12:09 +0200

    package utils kDoc

--- app/src/main/java/com/example/theweatherapp/utils/Converters.kt
+++ app/src/main/java/com/example/theweatherapp/utils/Converters.kt
@@ -4,28 +4,71 @@ import androidx.room.TypeConverter
 import com.google.gson.Gson
 import com.google.gson.reflect.TypeToken
 
+/**
+ * A class that provides type converters for Room database to handle serialization and deserialization
+ * of complex types such as lists of primitive types.
+ *
+ * Type converters are used by Room to convert complex data types into a format that can be stored
+ * in a SQLite database and vice versa.
+ */
 class Converters {
+    /**
+     * Converts a [List] of [Double] values to a [String] using Gson serialization.
+     *
+     * @param value The list of [Double] values to convert. It can be null.
+     * @return A [String] representation of the list in JSON format. If the input list is null, returns null.
+     */
     @TypeConverter
     fun fromDoubleList(value: List<Double>?): String = Gson().toJson(value)
 
+    /**
+     * Converts a [String] representing a JSON array to a [List] of [Double] values using Gson deserialization.
+     *
+     * @param value The JSON [String] to convert. It can be null.
+     * @return A [List] of [Double] values if deserialization is successful, or null if the input string is null.
+     */
     @TypeConverter
     fun toDoubleList(value: String): List<Double>? {
         val listType = object : TypeToken<List<Double>>() {}.type
         return Gson().fromJson(value, listType)
     }
 
+    /**
+     * Converts a [List] of [String] values to a [String] using Gson serialization.
+     *
+     * @param value The list of [String] values to convert. It can be null.
+     * @return A [String] representation of the list in JSON format. If the input list is null, returns null.
+     */
     @TypeConverter
     fun fromStringList(value: List<String>?): String = Gson().toJson(value)
 
+    /**
+     * Converts a [String] representing a JSON array to a [List] of [String] values using Gson deserialization.
+     *
+     * @param value The JSON [String] to convert. It can be null.
+     * @return A [List] of [String] values if deserialization is successful, or null if the input string is null.
+     */
     @TypeConverter
     fun toStringList(value: String): List<String>? {
         val listType = object : TypeToken<List<String>>() {}.type
         return Gson().fromJson(value, listType)
     }
 
+    /**
+     * Converts a [List] of [Int] values to a [String] using Gson serialization.
+     *
+     * @param value The list of [Int] values to convert. It can be null.
+     * @return A [String] representation of the list in JSON format. If the input list is null, returns null.
+     */
     @TypeConverter
     fun fromIntList(value: List<Int>?): String = Gson().toJson(value)
 
+    /**
+     * Converts a [String] representing a JSON array to a [List] of [Int] values using Gson deserialization.
+     *
+     * @param value The JSON [String] to convert. It can be null.
+     * @return A [List] of [Int] values if deserialization is successful, or null if the input string is null.
+     */
     @TypeConverter
     fun toIntList(value: String): List<Int>? {
         val listType = object : TypeToken<List<Int>>() {}.type
--- app/src/main/java/com/example/theweatherapp/utils/Response.kt
+++ app/src/main/java/com/example/theweatherapp/utils/Response.kt
@@ -1,12 +1,37 @@
 package com.example.theweatherapp.utils
 
+/**
+ * A sealed class representing the possible states of a response.
+ *
+ * This class is used to encapsulate different outcomes of an operation, typically
+ * for handling network requests or asynchronous operations. It provides a way to
+ * represent loading, successful results, and failure states in a type-safe manner.
+ *
+ * @param T The type of the data contained in the response. This is a generic type
+ *          that can be specified when creating instances of [Success].
+ */
 sealed class Response<out T> {
+    /**
+     * Represents the loading state of the response.
+     *
+     * This state indicates that the operation is in progress and data is not yet available.
+     */
     object Loading : Response<Nothing>()
 
+    /**
+     * Represents a successful response containing data.
+     *
+     * @property data The data retrieved from the operation. It can be null if no data is available.
+     */
     data class Success<out T>(
         val data: T?,
     ) : Response<T>()
 
+    /**
+     * Represents a failed response.
+     *
+     * @property e The exception that occurred during the operation, if any. It can be null if no exception was thrown.
+     */
     data class Failure(
         val e: Exception?,
     ) : Response<Nothing>()
